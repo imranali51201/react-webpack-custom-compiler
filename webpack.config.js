f624c168b86c16c766f3ea7e8e24a51d32d47425
Imran Ali <imranali51201@gmail.com>
2023-12-03 16:37:11 +0500

    Initialize

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 3000,
+    open: true,
+  },
+};
